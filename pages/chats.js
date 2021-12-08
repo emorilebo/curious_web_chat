1f56e59e74920382e99d2b3d5ea302a6c7de1766
emorilebo <emorylebo@gmail.com>
2021-12-08 14:05:49 +0100

    ADD chats screen

--- pages/chats.js
+++ pages/chats.js
@@ -1,4 +1,16 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
 
 export default function Chats() {
   return <div className="background">chats</div>;
