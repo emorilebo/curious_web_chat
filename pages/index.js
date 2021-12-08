0e4ea3f820db2fcea48205940f7e88454d24ee6c
emorilebo <emorylebo@gmail.com>
2021-12-08 13:34:46 +0100

    ADD: auth form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, {useContent} from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from 'axios';
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  return <div className="background">
+    <div className="auth-container">
+      <form className="auth-form" onSubmit={e=>e.preventDefault()} action="">
+        
+      </form>
+    </div>
+  </div>;
 }
