bb28719f494fd99adcd20c9d3f0a20c0e89de680
emorilebo <emorylebo@gmail.com>
2021-12-08 13:44:57 +0100

    UPDATE: login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {useContent} from "react";
+import React, {useContext} from "react";
 
 import { Context } from "../context";
 
@@ -7,10 +7,29 @@ import { useRouter } from "next/router";
 import axios from 'axios';
 
 export default function Auth() {
+  const {
+    setUsername,
+    setSecret,
+  } = useContext(Context);
   return <div className="background">
     <div className="auth-container">
       <form className="auth-form" onSubmit={e=>e.preventDefault()} action="">
-        
+        <div className="auth-title">Curious Chat</div>
+        <div className="input-container">
+          <input type="text" 
+            placeholder="Email"
+            className="text-input"
+            onChange={e=>setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <input type="password" 
+            placeholder="Password"
+            className="text-input"
+            onChange={e=>setSecret(e.target.value)}
+          />
+        </div>
+        <button type="submit" className='submit-button'>Login / Sign Up</button>
       </form>
     </div>
   </div>;
